016b8e7b21623b7325088760a569699868fcdd80
Andy608 <AndrewR608@gmail.com>
2015-05-14 21:37:20 -0400

    Continuing to work on the server.

--- src/com/compsci/core/PlayerConnectionHandler.java
+++ src/com/compsci/core/PlayerConnectionHandler.java
@@ -1,19 +1,36 @@
 package com.compsci.core;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
-public class PlayerConnectionHandler implements Runnable {
+public class PlayerConnectionHandler extends Thread {
 
-	private Socket playerConnection;
+	private Socket socket;
 	
 	public PlayerConnectionHandler(Socket incomingConnection) {
-		System.out.println("New user connected to the server!");
-		playerConnection = incomingConnection;
+		super("MultiplayerThread");
+		socket = incomingConnection;
 	}
 	
 	@Override
 	public void run() {
 		//"Game loop" will go in here.
 		//Constantly update until user disconnects
+		
+		try (PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
+			
+			out.println("Welcome to the server!");
+			
+			while () {
+				
+			}
+			
+		} catch (IOException e) {
+			
+		}
 	}
 }
--- src/com/compsci/core/SloverseServer.java
+++ src/com/compsci/core/SloverseServer.java
@@ -1,11 +1,7 @@
 package com.compsci.core;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,27 +9,21 @@ import com.compsci.entity.Player;
 
 public class SloverseServer {
 
-	private List<Player> connectedUsers;
+	private static boolean listening;
+	private static int portNumber = 608;
 	
-	public SloverseServer() {
-		
-		connectedUsers = new ArrayList<>();
-	}
+	private static List<Player> connectedUsers = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		
-		try (ServerSocket s = new ServerSocket(608);
-			Socket clientSocket = s.accept();
-			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
+		try (ServerSocket socket = new ServerSocket(portNumber);) {
 			
-			while (true) {
-				Runnable r = new PlayerConnectionHandler(clientSocket);
-				Thread t = new Thread(r);
-				t.start();
+			while (listening) {
+				new PlayerConnectionHandler(socket.accept()).start();
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.err.println("Could not listen to port: " + portNumber);
+			System.exit(-1);
 		}
 	}
 }
